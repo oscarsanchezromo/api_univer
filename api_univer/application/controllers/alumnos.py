a16d7ca637b7047685c8fd264c554fb0d063e77f
oscarsanchezromo <51491251+oscarsanchezromo@users.noreply.github.com>
2020-04-28 15:40:07 -0500

    Update alumnos.py

--- api_univer/application/controllers/alumnos.py
+++ api_univer/application/controllers/alumnos.py
@@ -1,172 +1,54 @@
-import web
-import app
-import json
-import csv
+import web 
+import app 
+import csv 
+import json 
+
 
 class Alumnos:
-	file="/static/csv/alumnos.csv"
-	version="1.0.0"
-	def __init__(self):
-		pass
 
-	def GET(self):
-		try:
-			datos=web.input()
-			if datos['token']=="1234": 
-				
-				if datos['action']=="get": 
-					resultAB=self.actionGet(self.version,self.file)
-					return json.dumps(resultAB) 
-				
-				elif datos['action']=="search":
-					matricula=datos['matricula']
-					resultAB=self.actionSearch(self.version,self.file,matricula)
-					return json.dumps(resultAB) 
-				
-				elif datos['action']=="put": #insertar
-					matricula=datos['matricula']
-					nombre=datos['nombre']
-					primer_apellido=datos['apellido1']
-					segundo_apellido=datos['apellido2']
-					carrera=datos['carrera']
-					resultAB=self.actionInput(self.version,self.file,matricula,nombre,primer_apellido,segundo_apellido,carrera)
-					return json.dumps(resultAB)#
-				
-				elif datos['action']=="delete":
-					matricula=datos['matricula']
-					resultAB=self.actionDelete(self.version,self.file,matricula)
-					return json.dumps(resultAB) 
-				else: 
-					resultAB={}
-					resultAB['status']="Command not match, try again"
-					return json.dumps(resultAB)
-			else: #por si el token es incorrecto
-				resultA={}
-				resultA['status']="Token invalido"
-				return json.dumps(resultA)
-		except Exception as e:
-			resultA={}
-			resultA['status']="Ingresa valores validos, revisa"
-			return json.dumps(resultA)
+    def __init__(self): 
+        pass 
 
-#GET
-	@staticmethod
-	def actionGet(version,file):
-		try:
-				resultA=[]
-				resultAB={}
-				with open('static/csv/alumnos.csv','r') as csvfile: #csv
-					reader = csv.DictReader(csvfile)
-					for row in reader: 
-						resultA.append(row)
-						resultAB['version']=version
-						resultAB['status']="200 ok" 
-						resultAB['alumnos']=resultA
-				return resultAB 
-		except Exception as e: 
-			resultA={}
-			resultA['version']=version
-			resultA['status']="Error"
-			return json.dumps(resultA)
-	
-#SEARCH
-	@staticmethod
-	def actionSearch(version,file,matricula):
-		try:
-				resultA=[]
-				resultAB={}
-				with open('static/csv/alumnos.csv','r') as csvfile: 
-					reader = csv.DictReader(csvfile)
-					for row in reader:
-						if(matricula==row['matricula']): 
-							print(matricula)
-							resultA.append(row) 
-							resultAB['version']=version
-							resultAB['status']="200 ok"
-							resultAB['alumnos']=resultA
-							break
-						else:
-							resultAB={}
-							resultAB['status']="matricula no encontrada" 
-				return resultAB 
-		except Exception as e: 
-			resultA={}
-			resultA['version']=version
-			resultA['status']="Error"
-			return json.dumps(resultA)
+    def GET(self):
+        try:
+            data = web.input() 
+            if data['token'] == "1234": 
+                if data['action'] == 'get': 
+                    result = self.actionGet() 
+                    return json.dumps(result) 
+                else:
+                    result = {} 
+                    result['status'] = "Command not found"
+                    return json.dumps(result) 
+            else:
+                result = {} 
+                result['status'] = "Invalid Token"
+                return json.dumps(result) 
+        except Exception as e:
+            result = {} 
+            result['status'] = "Values missing, sintaxis: alumnos?action=get&token=XXXX"
+            return json.dumps(result) 
 
-
-#INPUT
-	@staticmethod
-	def actionInput(version,file,matricula,nombre,primer_apellido,segundo_apellido,carrera):
-		try:
-			resultA=[]
-			resultAIN=[]
-			resultAB={}
-			resultAIN.append(matricula)
-			resultAIN.append(nombre)
-			resultAIN.append(primer_apellido)
-			resultAIN.append(segundo_apellido)
-			resultAIN.append(carrera)
-			with open('static/csv/alumnos.csv','a',newline='') as csvfile: 
-				writer=csv.writer(csvfile)
-				# The writerow() method writes a row of data into the specified file
-				writer.writerow(resultAIN)
-			with open('static/csv/alumnos.csv','r') as csvfile:
-				reader = csv.DictReader(csvfile)
-				for row in reader:
-					resultA.append(row)
-					resultAB['version']=version
-					resultAB['status']="200 ok" 
-					resultAB['alumnos']=resultA
-			return resultAB 
-		except Exception as e:
-			resultA={} 
-			resultA['version']=version
-			resultA['status']="Error"
-			return json.dumps(resultA)
-
-	@staticmethod
-	def actionDelete(file, matricula):
-		try:
-			result = []
-			resultC = {}
-			with open('static/csv/alumnos.csv', 'r') as csvfile:
-				reader = csv.DictReader(csvfile)
-				for row in reader:
-					if(row['matricula'] != matricula):
-						resultC['status'] = "200 Ok"
-						result.append(row)
-						resultC['alumnos'] = result
-			longitud = (len(result))
-			with open('static/csv/alumnos.csv', 'w', newline='') as csvfile:
-				writer = csv.writer(csvfile)
-				bye = []
-				bye.append("matricula")
-				bye.append("nombre")
-				bye.append("primer_apellido")
-				bye.append("segundo_apellido")
-				bye.append("carrera")
-				writer.writerow(bye)
-				data = []
-				for x in range(0, longitud):
-					data.append(result[x]['matricula'])
-					data.append(result[x]['nombre'])
-					data.append(result[x]['primer_apellido'])
-					data.append(result[x]['segundo_apellido'])
-					data.append(result[x]['carrera'])
-					writer.writerow(data)
-					data = []
-			results = []
-			resultsC = {}
-			with open('static/csv/alumnos.csv', 'r') as csvfile:
-				reader = csv.DictReader(csvfile)
-				for row in reader:
-					results.append(row)
-					resultsC['status'] = "200 Ok"
-					resultsC['alumnos'] = result
-			return resultsC
-		except Exception as e:
-			result = {}
-			result['status']="Error"
-		return result
+    @staticmethod
+    def actionGet():
+        try:
+            result = {} 
+            result['status'] = "200 Ok" 
+            file = 'static/csv/alumnos.csv' 
+            with open(file,'r') as csvfile: 
+                reader = csv.DictReader(csvfile) 
+                alumnos = [] 
+                for row in reader: 
+                    fila = {} 
+                    fila['matricula'] = row['matricula'] 
+                    fila['nombre'] = row['nombre'] 
+                    fila['primer_apellido'] = row['primer_apellido'] 
+                    fila['segundo_apellido'] = row['segundo_apellido'] 
+                    fila['carrera'] = row['carrera'] 
+                    alumnos.append(fila) 
+                result['alumnos'] = alumnos 
+            return result 
+        except  Exception as e:
+            result = {}
+            result['status'] = "Error"
+            return result
